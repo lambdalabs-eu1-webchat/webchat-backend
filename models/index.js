80c6d2b2dd238f056967f439caded4c866652ce2
Pav0l <pavol.karas@gmail.com>
2019-04-28 18:46:54 +0200

    Rename schemas folder to models

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const User = require('./usersSchema');
+
+// set mongoose options
+// useCreateIndex - true to use `createIndex()` instead of deprecated `ensureIndex()`
+mongoose.set('useCreateIndex', true);
+
+// enable logging collection methods + arguments to the console
+// mongoose.set('debug', true);
+const connectDb = () => {
+  return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+};
+
+// Add aditional resoure models in the object
+const models = { User };
+
+module.exports = {
+  connectDb,
+  models
+};
