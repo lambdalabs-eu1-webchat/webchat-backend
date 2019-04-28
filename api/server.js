17b99ef4cf8eb89229cc3ceb6376e2e4b1bebf04
Pav0l <pavol.karas@gmail.com>
2019-04-28 19:20:04 +0200

    Wire up middleware and routes with logger

--- api/server.js
+++ api/server.js
@@ -1,22 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-const userRoutes = require('../routes/userRoutes');
 
 const server = express();
 
-server.use(compression());
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+// import all middleware and pass server to index.js
+require('../middleware')(server);
 
-
-server.get('/', (req, res) => {
-  res.status(200).json({ message: 'API is live!' });
-});
-
-
-server.use(userRoutes);
+// import all routes and pass server to index.js
+require('../routes')(server);
 
 module.exports = server;
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const userRoutes = require('./userRoutes');
+const { logger, errorLogger } = require('../middleware/winston');
+const path = require('../utils/path');
+
+module.exports = server => {
+  // winston logger
+  server.use(logger);
+
+  // sanity check endpoint
+  server.get('/', (req, res) => {
+    res.status(200).json({ message: 'API is live!' });
+  });
+
+  // routes
+  server.use(path.users, userRoutes);
+
+
+
+  // error logger - must be last
+  server.use(errorLogger);
+};
