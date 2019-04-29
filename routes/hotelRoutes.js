1e6811fe5c9a57e506ad09e90b9321704633569a
mark-marshall <mark.marshallgp@gmail.com>
2019-04-29 12:37:57 +0100

    Added draft code for getHotelRooms (awaiting schema

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -6,15 +6,14 @@ const path = require('../utils/path');
 const { models } = require('../models/index');
 
 // COMPANY - created when a Super Admin USER type is created
-//     - [GET] Company info
+// [GET] Company info
 
 
 // [GET]
 // params: 0;
 // body: 0;
 // queryString: 0;
-// completePath: /hotel/
-
+// completePath: /hotel
 routes.get('/', async (req, res, next) => {
   const id = req.decodedToken.hotel_id;
   try {
@@ -22,7 +21,7 @@ routes.get('/', async (req, res, next) => {
     if (hotelInfo) {
       res.status(200).json(hotelInfo);
     } else {
-      res.status(404).json(error.getHotelInfo);
+      res.status(404).json(error.getHotel);
     }
   } catch (error) {
     next(error);
@@ -30,7 +29,27 @@ routes.get('/', async (req, res, next) => {
 });
 
 // ROOMS
-//     - [GET] room numbers
-//     - [POST] new room
-//     - [DELETE] room
-//     - [PUT] room numbers
+// [GET] room numbers
+// params: 0;
+// body: 0;
+// queryString: 0;
+// completePath: /hotel/rooms/
+routes.get('/rooms', async (req, res, next) => {
+    const id = req.decodedToken.hotel_id;
+    try {
+      const hotelInfo = await models.Hotel.where({ id });
+      if (hotelInfo) {
+        res.status(200).json(hotelInfo.rooms);
+      } else {
+        res.status(404).json(error.getHotel);
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
+
+// [POST] new room
+
+
+// [DELETE] room
+// [PUT] room numbers
--- utils/error.js
+++ utils/error.js
@@ -1,7 +1,7 @@
 const error = {
   getUsers: 'the users could not be retrieved',
   getUserById: 'the user could not be retrieved',
-  getHotelInfo: 'the hotel information could not be retrieved'
+  getHotel: 'information for this hotel could not be retrieved',
 };
 
 module.exports = error;
