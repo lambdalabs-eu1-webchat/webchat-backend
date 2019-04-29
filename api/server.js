e603cd048d949555dd09bed15aa1e757920f58f6
Pav0l <pavol.karas@gmail.com>
2019-04-29 20:22:22 +0200

    Revert to previous state

--- api/server.js
+++ api/server.js
@@ -6,12 +6,6 @@ const server = express();
 // https://nodejs.org/api/modules.html#modules_folders_as_modules
 require('../middleware')(server);
 
-server.get('/', (req, res) => {
-  res.status(200).json({ message: 'API works!' });
-});
-
-const userRoutes = require('./users/userRoutes');
-
-server.use(userRoutes);
+require('../routes')(server);
 
 module.exports = server;
