dcb5aebbae78f0f82a5d795be57fc80386215566
mark-marshall <mark.marshallgp@gmail.com>
2019-04-29 21:58:56 +0100

    Added db-format-aligment on name duplication check to prevent wrong-validation by case differences

--- middleware/validateHotelPost.js
+++ middleware/validateHotelPost.js
@@ -3,7 +3,10 @@ const error = require('../utils/error');
 
 const validateHotelPost = async (req, res, next) => {
   const hotel = req.body;
-  const hotelUniqueCheck = await models.Hotel.findOne({ name: hotel.name });
+  const hotelName = hotel.name
+    .toLowerCase()
+    .replace(/\b([a-z])/gi, char => char.toUpperCase());
+  const hotelUniqueCheck = await models.Hotel.findOne({ name: hotelName });
   if (!hotel.name || !hotel.motto) {
     res.status(400).json(error.invalidHotelPost);
   } else if (hotelUniqueCheck) {
