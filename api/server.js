2b71b146e4add3ca777c0856cf29ac23b94557db
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-29 20:10:14 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,16 +2,6 @@ const express = require('express');
 
 const server = express();
 
-server.use(async (req, res, next) => {
-  try {
-    req.context = {
-      models,
-    };
-    next();
-  } catch (error) {
-    console.error(error);
-  }
-});
 // import all middleware and pass server to index.js
 require('../middleware')(server);
 
