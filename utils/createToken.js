254b9af3fd547eeaa04519ca1a054adad495185f
Pav0l <pavol.karas@gmail.com>
2019-04-29 15:59:49 +0200

    Build auth middleware with JWT

---
+++ utils/createToken.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const { super_secret } = require('../utils/secrets');
+
+const createToken = ({ user_id, username, hotel_id, chat_id }) =>
+  jwt.sign(
+    // token payload
+    { payload: user_id, username, hotel_id, chat_id },
+    // token secret
+    process.env.JWT_SECRET || super_secret
+  );
+
+module.exports = createToken;
---
+++ utils/secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+  super_secret: 'There_Is_N0_S3cr3t_So_Stop_Looking',
+};
