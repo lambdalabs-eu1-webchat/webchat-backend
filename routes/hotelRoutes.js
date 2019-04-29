8452ed67cfba4c09406d31844240714bb570ab88
mark-marshall <mark.marshallgp@gmail.com>
2019-04-29 13:39:01 +0100

    Added draft code for postHotel (awaiting schema

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -5,15 +5,32 @@ const error = require('../utils/error');
 const path = require('../utils/path');
 const { models } = require('../models/index');
 
-// COMPANY - created when a Super Admin USER type is created
-// [GET] Company info
+// HOTEL - created when a Super Admin USER type is created
+// [POST] new hotel
+// params: 0;
+// body: valid hotel object
+// queryString: 0;
+// Path: /hotel
+routes.post('/', async (req, res, next) => {
+  const hotel = req.body;
+  try {
+    const newHotel = new models.Hotel(hotel);
+    await newHotel.save();
+    if (newHotel) {
+      res.status(201).json(newHotel);
+    } else {
+      res.status(400).json(error.addHotel);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-
-// [GET]
+// [GET] hotel
 // params: 0;
 // body: 0;
 // queryString: 0;
-// completePath: /hotel
+// Path: /hotel
 routes.get('/', async (req, res, next) => {
   const id = req.decodedToken.hotel_id;
   try {
@@ -33,23 +50,35 @@ routes.get('/', async (req, res, next) => {
 // params: 0;
 // body: 0;
 // queryString: 0;
-// completePath: /hotel/rooms/
+// Path: /hotel/rooms/
 routes.get('/rooms', async (req, res, next) => {
-    const id = req.decodedToken.hotel_id;
-    try {
-      const hotelInfo = await models.Hotel.where({ id });
-      if (hotelInfo) {
-        res.status(200).json(hotelInfo.rooms);
-      } else {
-        res.status(404).json(error.getHotel);
-      }
-    } catch (error) {
-      next(error);
+  const id = req.decodedToken.hotel_id;
+  try {
+    const hotelInfo = await models.Hotel.where({ id });
+    if (hotelInfo) {
+      res.status(200).json(hotelInfo.rooms);
+    } else {
+      res.status(404).json(error.getHotel);
     }
-  });
+  } catch (error) {
+    next(error);
+  }
+});
 
 // [POST] new room
-
+routes.post('/rooms', async (req, res, next) => {
+  const hotel = req.body;
+  try {
+    const addedHotel = await new models.Hotel(hotel);
+    if (addedHotel) {
+      res.status(201).json(addedHotel);
+    } else {
+      res.status(404).json(error.addHotel);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 // [DELETE] room
 // [PUT] room numbers
--- utils/error.js
+++ utils/error.js
@@ -2,6 +2,7 @@ const error = {
   getUsers: 'the users could not be retrieved',
   getUserById: 'the user could not be retrieved',
   getHotel: 'information for this hotel could not be retrieved',
+  addHotel: 'the hotel could not be added',
 };
 
 module.exports = error;
