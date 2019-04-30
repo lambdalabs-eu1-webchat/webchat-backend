688591037590f7c76dcd3dc7011f4694e92e8a71
mark-marshall <mark.marshallgp@gmail.com>
2019-04-30 13:51:46 +0100

    Reformatted errorMessages

--- utils/errorMessage.js
+++ utils/errorMessage.js
@@ -5,28 +5,28 @@ const errorMessage = {
   duplicateEmail: { message: 'This email is taken already.' },
   updateUser: { message: 'The user could not be updated' },
   deleteUser: { message: 'The user could not be deleted' },
-  addUser: { message: 'The user could not be added' },
-  noHotel: { message: 'no hotel exists with this id' },
-  addHotel: { message: 'the hotel could not be added' },
+  addUser: { message: 'The user could not be added.' },
+  noHotel: { message: 'No hotel exists with this id.' },
+  addHotel: { message: 'The hotel could not be added.' },
   updateHotel: {
     message:
-      'the hotel could not be updated or you did not provide any new information',
+      'The hotel could not be updated or you did not provide any new information.',
   },
-  addRoom: { message: 'the room could not be added' },
-  updateRoom: { message: 'the room could not be updated' },
-  removeRoom: { message: 'the room could not be deleted' },
-  invalidObjectId: { message: 'an invalid ObjectId was passed' },
+  addRoom: { message: 'The room could not be added.' },
+  updateRoom: { message: 'The room could not be updated.' },
+  removeRoom: { message: 'Rhe room could not be deleted.' },
+  invalidObjectId: { message: 'An invalid ObjectId was passed.' },
   invalidHotelPost: {
-    message: 'a hotel must be added with at least a name and motto',
+    message: 'A hotel must be added with at least a name and motto.',
   },
   invalidHotelPut: {
-    message: 'a hotel must be changed with at least an updated name or motto',
+    message: 'A hotel must be changed with at least an updated name or motto.',
   },
   restrictedHotelPut: {
-    message: 'hotel rooms cannot be ammended via this endpoint',
+    message: 'Hotel rooms cannot be ammended via this endpoint.',
   },
   duplicateHotel: {
-    message: 'a hotel with this name already exists',
+    message: 'A hotel with this name already exists.',
   },
 };
 
