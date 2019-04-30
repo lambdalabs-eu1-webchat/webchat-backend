501dc038d350f758bba946c17976a801a0f7fd74
conconrob <connortprobertson@gmail.com>
2019-04-30 16:28:11 +0100

    did hotels test

---
+++ tests/mongoose/hotels.test.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Hotel = require('../../models/hotelModels');
+//  process.env.TEST_SUITE = 'hotels-test';
+
+describe('Hotel Model', () => {
+  it('should return a error if rooms is not an array', done => {
+    const hotel = new Hotel({ rooms: 1, name: 'West', motto: 'a motto' });
+    hotel.validate(res => {
+      console.log(res.errors['rooms'].message);
+      expect(res._message).toBe('Hotels validation failed');
+      done();
+    });
+  });
+  it('should return error if no name given', done => {
+    const hotel = new Hotel({ rooms: [], motto: 'a motto' });
+    hotel.validate(res => {
+      expect(res.errors['name'].message).toBe('Name is required');
+      done();
+    });
+  });
+});
