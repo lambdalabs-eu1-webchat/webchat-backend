121e0617785cd04c893b7f32f92e18868495fb2f
Pav0l <pavol.karas@gmail.com>
2019-05-01 13:30:51 +0200

    Create helper function to validate users props

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,4 +1,9 @@
-module.exports = { remainder, randomMinMax, isEmail };
+module.exports = {
+  remainder,
+  randomMinMax,
+  isEmail,
+  updateUser,
+};
 
 function remainder(numerator, denominator) {
   const num = numerator / denominator;
@@ -11,6 +16,14 @@ function randomMinMax(min, max) {
 
 function isEmail(email) {
   return email.test(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
+}
+
+function updateUser(current, newUser) {
+  if (newUser.name) current.name = newUser.name;
+  if (newUser.email) current.email = newUser.email;
+  if (newUser.password) current.password = newUser.password;
+  if (newUser.motto) current.motto = newUser.motto;
+  if (newUser.user_type) current.user_type = newUser.user_type;
 }
