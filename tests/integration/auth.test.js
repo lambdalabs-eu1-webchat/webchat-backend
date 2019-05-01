26fc18dbfeb039246c6b23ee9db4cef7ec68e272
mark-marshall <mark.marshallgp@gmail.com>
2019-05-01 11:10:47 +0100

    Switched harcoded url in auth.test to env variable aand added dotenv require

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,13 +1,14 @@
 const request = require('supertest');
 const server = require('../../api/server');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 describe('/api/auth', () => {
   let connection;
   let db;
 
   beforeAll(async () => {
-    connection = await mongoose.connect('mongodb://localhost:27017/jest', {
+    connection = await mongoose.connect(process.env.JEST_DATABASE_URL, {
       useNewUrlParser: true,
     });
     const db = mongoose.model('users', {});
