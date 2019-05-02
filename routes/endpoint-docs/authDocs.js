eadbfa7c22ab3cac6ada06a9c74264d39b7a1819
Talent Antonio <antonio.talenty@gmail.com>
2019-05-02 08:26:42 +0100

    Updates from comments

--- routes/endpoint-docs/authDocs.js
+++ routes/endpoint-docs/authDocs.js
@@ -7,9 +7,12 @@
  *  @apiParam {json} user information
  *  @apiParamExample {json} Request-Example:
  *    {
- *      "name": "nicolas group",
- *      " password": "1234",
+ *        "name": "nicolas group",
+ *        " password": "1234",
  *        "email": "nicolasgroup@gmail.com"
+ *        "hotel_id": "5cc74ab1f16ec37bc8cc4cdb",
+ *        "motto": "Cross-platform executive application",
+ *        "user_type": "receptionist"
  *    }
  *  @apiSuccess {String} name
  * @apiSuccess {String} _id  Unique id of the user.
@@ -19,11 +22,17 @@
  *  @apiSuccessExample Success-Response: registered
  *    HTTP/1.1 200 OK
  *    {
- *       "_id":userId"5cc9839d0b92251d9ab4a1f4",
- *      "name":"Merle",
- *      "email": "nicolasgroup@gmail.com",
- *     "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibmljb2xhcyBncm91cCIsImlhdCI6MTU1NjcxMDMwMX0.RdXMwTg3AMjiquiacVVMWL_kyUTOgdjTZLTdrPxzfoA"
- *    }
+ *      "user": {
+ *      "_id": "5cc9b28740daa6367c5d0283",
+ *      "hotel_id": "5cc74ab1f16ec37bc8cc4cdb",
+ *      "name": "blabla",
+ *      "email": "aazzaa@hotmail.com",
+ *      "motto": "Cross-platform executive application",
+ *      "user_type": "receptionist",
+ *       "__v": 0
+ *  },
+ *     "token":     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYmxhYmxhIiwiaG90ZWxfaWQiOiI1Y2M3NGFiMWYxNmVjMzdiYzhjYzRjZGIiLCJpYXQiOjE1NTY3MjIzMTF9.zI9RKAe7Gr8kfctrMH_51ixu2P0v6yNQXhPZfwo6m8I"
+ * }
  *
   @apiErrorExample Error-Response: minimum register requirements
  *    HTTP/1.1 401 BAD REQUEST
