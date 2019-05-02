1921395afcc480375c98e73fad322d5bb6aefb61
Talent Antonio <antonio.talenty@gmail.com>
2019-05-02 13:37:06 +0100

    Delete Room with ID

--- routes/endpoint-docs/roomRoutesDocs.js
+++ routes/endpoint-docs/roomRoutesDocs.js
@@ -75,3 +75,45 @@
  *    }
  */
 
+ /**
+ *  @api {delete} api/:_id room / id  update room Id 
+ *  @apiVersion 0.1.0
+ *  @apiName delete rooms
+ *  @apiGroup Rooms
+ *
+ *  @apiParam {String}  room Id 
+ * @apiParam {String}  room name
+ *  @apiParamExample {json} Request-Example:
+ *    {
+ *      "id": "5cc95fa83de71d13395320fa",
+ *       "name": "1"
+ *    }
+ * 
+ *  @apiSuccess {String} _id of the updated room
+ *  @apiSuccess {String} name of the updated room
+ *
+ *  @apiSuccessExample Success-Response: Room Array
+ *    HTTP/1.1 200 OK
+  * [  
+ *   The deleted room will be missing which has name if "1"
+ *   {
+ *   "_id": "5cc95fa83de71d13395320fb",
+ *   "name": "2",
+ *    }
+ *   {
+ *   "_id": "5cc95fa83de71d13395320fc",
+ *   "name": "3",
+ *    }
+ *   {
+ *   "_id": "5cc95fa83de71d13395320fd",
+ *   "name": "4",
+ *    }
+ * ]  ..............till the last room
+ * 
+ *  @apiErrorExample Error-Response: Wrong Room id or non existing room id
+ *    HTTP/1.1 400 BAD REQUEST
+ *    {
+ *        "message": "No hotel exists with this id."
+ *    }
+ */
+
