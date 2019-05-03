8151ce915874a204a961907aed3b6a1a0a7983d2
Pav0l <pavol.karas@gmail.com>
2019-05-03 16:18:42 +0200

    Plug in chat auth into routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const userRoutes = require('./userRoutes');
 const hotelRoutes = require('./hotelRoutes');
 const roomRoutes = require('./roomRoutes');
+const chatAuth = require('./chatAuth');
 const { logger, errorLogger } = require('../middleware/winston');
 const path = require('../utils/path');
 const auth = require('./auth');
@@ -18,6 +19,7 @@ module.exports = server => {
 
   // routes
   server.use(path.auth, auth);
+  server.use(path.users, chatAuth);
   server.use(path.users, userRoutes);
   server.use(path.hotel, hotelRoutes);
   server.use(path.hotel, roomRoutes);
