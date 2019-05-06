b86037039faf98ca5926e49e64be6abc92982912
conconrob <connortprobertson@gmail.com>
2019-05-06 15:14:13 +0100

    fixed small bugs in close ticket

--- routes/chats/ratingFunction.js
+++ routes/chats/ratingFunction.js
@@ -1,16 +1,15 @@
-module.exports = rating;
+module.exports = makeRating;
 
 const { models } = require('../../models/index');
 
-async function rating(rating, ticket_id, socket, io) {
+async function makeRating(rating, socket) {
+  console.log('++++++++++++++++++++HERE=======================');
   const { user } = socket;
   // get the chat
   const chat = await models.Chat.findOne({ 'guest.id': user._id });
   // update the rating for the ticket
-  chat.tickets.map(ticket => {
-    if (!ticket._id.equals(ticket_id)) return ticket;
-    return { ...ticket, rating };
-  });
+  chat.tickets[chat.tickets.length - 1].rating = rating;
+
   // save the chat to the guest
   chat.save(error => {
     if (error) {
