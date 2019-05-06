5507ae3ae3e099de5c3a3c9b8b98c1adf0109a67
Pav0l <pavol.karas@gmail.com>
2019-05-06 09:57:13 +0200

    Update async tests

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -21,7 +21,7 @@ describe('/api/auth', () => {
     await db.deleteMany({});
   });
 
-  it('is in the right environment', async () => {
+  it('is in the right environment', () => {
     expect(process.env.NODE_ENV).toBe('test');
   });
 
@@ -69,7 +69,7 @@ describe('/api/auth', () => {
         .expect(422);
     });
 
-    it('should return return the user object without password', async () => {
+    it('should return return the user object without password', async done => {
       const newUser4 = {
         hotel_id: '5cc74ab1f16ec37bc8cc4cdb',
         name: 'Frank',
@@ -83,6 +83,7 @@ describe('/api/auth', () => {
         .send(newUser4);
 
       expect(createdUser.text).not.toHaveProperty('password');
+      done();
     });
   });
 
@@ -109,7 +110,7 @@ describe('/api/auth', () => {
         .expect(401);
     });
 
-    it('should return return the user object without password', async () => {
+    it('should return return the user object without password', async done => {
       const newUser = {
         name: 'Joe',
         password: '1234',
@@ -119,6 +120,7 @@ describe('/api/auth', () => {
         .send(newUser);
 
       expect(createdUser.text).not.toHaveProperty('password');
+      done();
     });
   });
 });
