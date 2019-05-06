e69ddc53e436c07c0d73ec2d4664a1fac9f28f84
Pav0l <pavol.karas@gmail.com>
2019-05-06 10:58:53 +0200

    Update docs for auth

--- routes/endpoint-docs/authDocs.js
+++ routes/endpoint-docs/authDocs.js
@@ -8,11 +8,9 @@
  *  @apiParamExample {json} Request-Example:
  *    {
  *        "name": "nicolas group",
- *        " password": "1234",
+ *        "password": "1234",
  *        "email": "nicolasgroup@gmail.com"
- *        "hotel_id": "5cc74ab1f16ec37bc8cc4cdb",
- *        "motto": "Cross-platform executive application",
- *        "user_type": "receptionist"
+ *        "motto": "Cross-platform executive application"
  *    }
  *  @apiSuccess {String} name
  * @apiSuccess {String} _id  Unique id of the user.
@@ -23,15 +21,15 @@
  *    HTTP/1.1 200 OK
  *    {
  *      "user": {
- *      "_id": "5cc9b28740daa6367c5d0283",
- *      "hotel_id": "5cc74ab1f16ec37bc8cc4cdb",
- *      "name": "blabla",
- *      "email": "aazzaa@hotmail.com",
- *      "motto": "Cross-platform executive application",
- *      "user_type": "receptionist",
- *       "__v": 0
- *  },
- *     "token":     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYmxhYmxhIiwiaG90ZWxfaWQiOiI1Y2M3NGFiMWYxNmVjMzdiYzhjYzRjZGIiLCJpYXQiOjE1NTY3MjIzMTF9.zI9RKAe7Gr8kfctrMH_51ixu2P0v6yNQXhPZfwo6m8I"
+ *         "_id": "5cc9b28740daa6367c5d0283",
+ *         "hotel_id": "5cc74ab1f16ec37bc8cc4cdb",
+ *         "name": "blabla",
+ *         "email": "aazzaa@hotmail.com",
+ *         "motto": "Cross-platform executive application",
+ *         "user_type": "Super Admin",
+ *           "__v": 0
+ *      },
+ *     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYmxhYmxhIiwiaG90ZWxfaWQiOiI1Y2M3NGFiMWYxNmVjMzdiYzhjYzRjZGIiLCJpYXQiOjE1NTY3MjIzMTF9.zI9RKAe7Gr8kfctrMH_51ixu2P0v6yNQXhPZfwo6m8I"
  * }
  *
   @apiErrorExample Error-Response: minimum register requirements
@@ -41,8 +39,7 @@
  *    }
  */
 
-
- /**
+/**
  *  @api {post} api/login
  *  @apiVersion 0.1.0
  *  @apiName postAuth
