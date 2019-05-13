0d8e1a4ecef73365b77ad7018baca2f7668b10e4
Pav0l <pavol.karas@gmail.com>
2019-05-13 14:31:31 +0200

    Add passcode to token payload

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 const { super_secret } = require('../utils/secrets');
 
-const createToken = ({ id, name, hotel_id }) =>
+const createToken = ({ id, name, hotel_id, passcode }) =>
   jwt.sign(
     // token payload
-    { payload: id, name, hotel_id },
+    { payload: id, name, hotel_id, passcode },
     // token secret
     process.env.JWT_SECRET || super_secret
   );
