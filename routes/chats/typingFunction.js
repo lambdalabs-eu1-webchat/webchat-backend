d161b0e38f24d8ca03189d95a19e32ccde9fad6e
conconrob <connortprobertson@gmail.com>
2019-05-13 12:47:54 +0100

    current typer now handles active and queued

--- routes/chats/typingFunction.js
+++ routes/chats/typingFunction.js
@@ -1,16 +1,28 @@
 module.exports = { userTyping, userStoppedTyping };
 
 const { TYPING, STOPPED_TYPING } = require('./constants');
-
+const { GUEST } = require('../../utils/USER_TYPES');
 async function userTyping(chat_id, socket, io) {
   const { user } = socket;
-  io.in(chat_id).emit(TYPING, {
-    user,
-    chat_id,
-  });
+  if (user.user_type === GUEST) {
+    // send it to the staff
+    io.in(user.hotel_id).emit(TYPING, {
+      user,
+      chat_id,
+    });
+  } else {
+    io.in(chat_id).emit(TYPING, {
+      user,
+      chat_id,
+    });
+  }
 }
 
 async function userStoppedTyping(chat_id, socket, io) {
   const { user } = socket;
-  io.in(chat_id).emit(STOPPED_TYPING, { user, chat_id });
+  if (user.user_type === GUEST) {
+    io.in(user.hotel_id).emit(STOPPED_TYPING, { user, chat_id });
+  } else {
+    io.in(chat_id).emit(STOPPED_TYPING, { user, chat_id });
+  }
 }
