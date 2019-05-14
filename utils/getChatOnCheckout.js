712eb4b77e7e3dc74afd53e44ab53c8a43eddac8
mark-marshall <mark.marshallgp@gmail.com>
2019-05-14 10:06:18 +0100

    Returned axios requests to deployed url

--- utils/getChatOnCheckout.js
+++ utils/getChatOnCheckout.js
@@ -4,10 +4,10 @@ const url = require('../utils/url');
 
 const getChatOnCheckout = async guestId => {
   try {
-    const chat = await axios.get(
-      `http://localhost:7000${path.chats}/checkout/${guestId}`,
-    );
-    // const chat = await axios.get(`${url}${path.chats}/checkout/${guestId}`);
+    // const chat = await axios.get(
+    //   `http://localhost:7000${path.chats}/checkout/${guestId}`,
+    // );
+    const chat = await axios.get(`${url}${path.chats}/checkout/${guestId}`);
     return chat.data;
   } catch (error) {
     console.error(error);
--- utils/getGuest.js
+++ utils/getGuest.js
@@ -4,10 +4,10 @@ const url = require('./url');
 
 const getGuest = async guestId => {
   try {
-    const guest = await axios.get(
-      `http://localhost:7000${path.users}/${guestId}`,
-    );
-    // const guest = await axios.get(`${url}${path.users}/${guestId}`);
+    // const guest = await axios.get(
+    //   `http://localhost:7000${path.users}/${guestId}`,
+    // );
+    const guest = await axios.get(`${url}${path.users}/${guestId}`);
     return guest.data;
   } catch (error) {
     console.error(error);
