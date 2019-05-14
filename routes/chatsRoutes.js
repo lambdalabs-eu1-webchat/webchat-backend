f20e8900011b6b7684a269b0aa730c0734a036d3
Pav0l <pavol.karas@gmail.com>
2019-05-14 12:12:12 +0200

    Create endpoints to translate chat

--- routes/chatsRoutes.js
+++ routes/chatsRoutes.js
@@ -79,4 +79,52 @@ routes.get('/', async (req, res, next) => {
   }
 });
 
+/**
+ * 1. Guest will send a message in non-english language
+ * 2. Receptionist will receive this message and clicks `Translate` button
+ * 3. The translated message with language name will show up
+ * @question The language for that ticket will be saved in Tickets Schema
+ * @question IF the language is not saved in Tickets schema, the receptionist will choose a language
+ * 5. Receptionist will respond to the message in English and click `Translate and send` button
+ * 6. The request will send hotel_id, message text, language used by guest to an endpoint, which will translate it to that language
+ * 6. Guest will receive the message in his used language
+ */
+
+const {
+  translate,
+  getLanguages,
+  translateToEnglish,
+} = require('../api/translate');
+routes.post('/translate', async (req, res, next) => {
+  try {
+    const { text, language, ticket_id } = req.body;
+    const hotel_id = req.query.hotel_id;
+
+    const toEnglish = await translateToEnglish(text);
+
+    const options = { runValidators: true };
+    const chats = await models.Chat.findByIdAndUpdate(
+      { hotel_id, 'tickets._id': ticket_id },
+      {
+        language: toEnglish[0].inputLang,
+      },
+      options
+    );
+
+    // const translatedText = await translate(text, language);
+    res.send(toEnglish);
+  } catch (error) {
+    next(error);
+  }
+});
+
+routes.get('/languages', async (req, res, next) => {
+  try {
+    const avalLanguages = await getLanguages();
+    res.send(avalLanguages);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = routes;
