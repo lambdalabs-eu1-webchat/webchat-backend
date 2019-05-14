5ff23177ad9a611331de0a17a398acdddedd0c2a
Pav0l <pavol.karas@gmail.com>
2019-05-14 12:12:37 +0200

    Create functions to interact with Translate API

--- api/translate.js
+++ api/translate.js
@@ -1,26 +1,92 @@
 // Imports the Google Cloud client library
 const { Translate } = require('@google-cloud/translate');
 
-async function quickstart(
-  projectId = process.env.PROJECT_ID // Your GCP Project Id
-) {
+async function translate(textToTranslate, targetLanguage) {
   try {
+    const projectId = process.env.PROJECT_ID; // Project Id from JSON file
     // Instantiates a client
     const translate = new Translate({ projectId });
 
-    // The text to translate
-    const text = 'Hello, world!';
+    // Detects the language. "textToTranslate" can be an array of strings for detecting the languages
+    // of multiple texts.
+    let [detections] = await translate.detect(textToTranslate);
+    detections = Array.isArray(detections) ? detections : [detections];
 
-    // The target language
-    const target = 'sk';
+    const translatedTextPromise = detections.map(async detection => {
+      const { input, confidence, language } = detection;
 
-    // Translates some text into Slovak
-    const [translation] = await translate.translate(text, target);
-    console.log(`Text: ${text}`);
-    console.log(`Translation: ${translation}`);
+      const translateOptions = {
+        to: targetLanguage.code,
+        model: 'nmt', // specify Neural Machine Translation (NMT) model
+      };
+
+      // take every detected language from "textToTranslate" and translate it to english
+      const [translation] = await translate.translate(input, translateOptions);
+      return {
+        translation,
+        input,
+        confidence,
+        inputLang: language,
+        translationLang: translateOptions.to,
+      };
+    });
+    const translatedText = await Promise.all(translatedTextPromise);
+
+    return translatedText;
   } catch (error) {
     console.error(error);
   }
 }
 
-quickstart();
+async function getLanguages() {
+  try {
+    const projectId = process.env.PROJECT_ID; // Project Id from JSON file
+    // Instantiates a client
+    const translate = new Translate({ projectId });
+
+    // Lists available translation language with their names in English (the default).
+    let [languages] = await translate.getLanguages();
+
+    return languages;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function translateToEnglish(textToTranslate) {
+  try {
+    const projectId = process.env.PROJECT_ID;
+    const translate = new Translate({ projectId });
+
+    // Detects the language. "textToTranslate" can be an array of strings for detecting the languages
+    // of multiple texts.
+    let [detections] = await translate.detect(textToTranslate);
+    detections = Array.isArray(detections) ? detections : [detections];
+
+    const translatedTextPromise = detections.map(async detection => {
+      const { input, confidence, language } = detection;
+
+      const translateOptions = {
+        to: 'en',
+        model: 'nmt', // specify Neural Machine Translation (NMT) model
+      };
+
+      // take every detected language from "textToTranslate" and translate it to english
+      const [translation] = await translate.translate(input, translateOptions);
+      return {
+        translation,
+        input,
+        confidence,
+        inputLang: language,
+        translationLang: translateOptions.to,
+      };
+    });
+    const translatedText = await Promise.all(translatedTextPromise);
+
+    return translatedText;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = { translate, getLanguages, translateToEnglish };
