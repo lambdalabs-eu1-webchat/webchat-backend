8fb22b6afda566770bff0651397ffd2096077f20
Pav0l <pavol.karas@gmail.com>
2019-05-21 12:52:13 +0200

    Restrict hotel routes

--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const restricted = require('express-restricted');
 const routes = express.Router();
 
+const { config, access } = require('../config/restricted');
 const errorMessage = require('../utils/errorMessage');
 const { models } = require('../models/index');
 const validateObjectId = require('../middleware/validateObjectId');
@@ -9,25 +11,31 @@ const validateHotelChange = require('../middleware/validateHotelChange');
 const { updateHotel } = require('../utils/helperFunctions');
 const { GUEST } = require('../utils/USER_TYPES');
 
-routes.get('/:_id', validateObjectId, async (req, res, next) => {
-  const { _id } = req.params;
-  try {
-    const hotelInfo = await models.Hotel.findById(_id);
-    if (hotelInfo) {
-      res.status(200).json(hotelInfo);
-    } else {
-      res.status(400).json(errorMessage.noHotel);
+routes.get(
+  '/:_id',
+  validateObjectId,
+  restricted(config, access.hotelStaff),
+  async (req, res, next) => {
+    const { _id } = req.params;
+    try {
+      const hotelInfo = await models.Hotel.findById(_id);
+      if (hotelInfo) {
+        res.status(200).json(hotelInfo);
+      } else {
+        res.status(400).json(errorMessage.noHotel);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 routes.put(
   '/:_id',
   validateObjectId,
   formatHotel,
   validateHotelChange,
+  restricted(config, access.super_admin),
   async (req, res, next) => {
     try {
       const { _id } = req.params;
@@ -45,25 +53,30 @@ routes.put(
     } catch (error) {
       next(error);
     }
-  },
+  }
 );
 //?status=here or ?status=left
 // gets the hotels guests
-routes.get('/:_id/guests', validateObjectId, async (req, res, next) => {
-  const { _id } = req.params;
-  const { status } = req.query;
-  const query = { hotel_id: _id, user_type: GUEST };
-  if (status) query.is_left = 'left' === status;
-  try {
-    const guests = await models.User.find(query);
-    if (guests) {
-      res.status(200).json(guests);
-    } else {
-      res.status(400).json(errorMessage.getUsers);
+routes.get(
+  '/:_id/guests',
+  validateObjectId,
+  restricted(config, access.hotelStaff),
+  async (req, res, next) => {
+    const { _id } = req.params;
+    const { status } = req.query;
+    const query = { hotel_id: _id, user_type: GUEST };
+    if (status) query.is_left = 'left' === status;
+    try {
+      const guests = await models.User.find(query);
+      if (guests) {
+        res.status(200).json(guests);
+      } else {
+        res.status(400).json(errorMessage.getUsers);
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 module.exports = routes;
