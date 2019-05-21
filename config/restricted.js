c455672dc95357d374f75424cc09130b06a8ad2c
Pav0l <pavol.karas@gmail.com>
2019-05-21 10:53:03 +0200

    Prepare different accesses

--- config/restricted.js
+++ config/restricted.js
@@ -1,10 +1,23 @@
 const { super_secret } = require('../utils/secrets');
+const {
+  SUPER_ADMIN,
+  ADMIN,
+  RECEPTIONIST,
+  GUEST
+} = require('../utils/USER_TYPES');
+
+const hotelStaff = [SUPER_ADMIN, ADMIN, RECEPTIONIST];
+const admins = [SUPER_ADMIN, ADMIN];
+const users = [SUPER_ADMIN, ADMIN, RECEPTIONIST, GUEST];
 
 module.exports = {
-  restricted: {
+  config: {
     reqProp: 'headers',
     childProp: 'authorization',
     identifier: 'user_type',
     jwtKey: process.env.JWT_SECRET || super_secret
+  },
+  access: {
+    email: hotelStaff
   }
 };
