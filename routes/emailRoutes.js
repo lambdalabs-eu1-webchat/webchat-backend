53bf4b3719405a0193e2e29d8528b548878ca7b4
Pav0l <pavol.karas@gmail.com>
2019-05-21 12:51:16 +0200

    Restrict email routes

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -14,35 +14,39 @@ const getChatOnCheckout = require('../utils/getChatOnCheckout');
 const formatChatOnCheckout = require('../utils/formatChatOnCheckout');
 const createEmail = require('../utils/createEmail');
 
-routes.post('/', restricted(config, access.email), async (req, res, next) => {
-  const { guestId, guestEmail, hotelId } = req.body;
+routes.post(
+  '/',
+  restricted(config, access.hotelStaff),
+  async (req, res, next) => {
+    const { guestId, guestEmail, hotelId } = req.body;
 
-  try {
-    if (validateEmail(guestEmail)) {
-      const guest = await getGuest(guestId);
-      const hotel = await getHotel(hotelId);
-      const [rawChatHistory] = await getChatOnCheckout(guestId);
-      if (rawChatHistory) {
-        const formattedChats = formatChatOnCheckout(rawChatHistory);
-        const emailBody = createEmail(guest, hotel, formattedChats);
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        const msg = {
-          to: guestEmail,
-          from,
-          subject: `${subject} ${hotel.name}`,
-          html: emailBody
-        };
-        await sgMail.send(msg);
-        res.status(200).json(response.sendChatLog);
+    try {
+      if (validateEmail(guestEmail)) {
+        const guest = await getGuest(guestId);
+        const hotel = await getHotel(hotelId);
+        const [rawChatHistory] = await getChatOnCheckout(guestId);
+        if (rawChatHistory) {
+          const formattedChats = formatChatOnCheckout(rawChatHistory);
+          const emailBody = createEmail(guest, hotel, formattedChats);
+          sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+          const msg = {
+            to: guestEmail,
+            from,
+            subject: `${subject} ${hotel.name}`,
+            html: emailBody
+          };
+          await sgMail.send(msg);
+          res.status(200).json(response.sendChatLog);
+        } else {
+          res.status(200).json(rawChatHistory);
+        }
       } else {
-        res.status(200).json(rawChatHistory);
+        res.status(400).json(errorMessage.invalidEmail);
       }
-    } else {
-      res.status(400).json(errorMessage.invalidEmail);
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 module.exports = routes;
