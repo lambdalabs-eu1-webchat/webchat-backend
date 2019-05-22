85bb105f10965b7a6f626f62cad416691da523af
Pav0l <pavol.karas@gmail.com>
2019-05-22 21:10:40 +0200

    Allow access to users by admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,41 +15,37 @@ const createPasscode = require('../utils/createPassCode');
 const createToken = require('../utils/createToken');
 const documentExists = require('../utils/documentExists');
 
-routes.get(
-  '/',
-  restricted(config, access.superAdmin),
-  async (req, res, next) => {
-    try {
-      const hotelId = req.query.hotel_id;
-      if (hotelId) {
-        const hotel = await models.Hotel.findById(hotelId);
-        if (hotel) {
-          // if hotel id exists, find all users with that hotel ID and return them
-          const hotelUsers = await models.User.where({
-            hotel_id: hotelId
-          }).where({ is_left: false });
-          // filter only hotel staff
-          const hotelStaff = hotelUsers.filter(
-            user => user.user_type !== USER_TYPES.GUEST
-          );
-          res.status(200).json(hotelStaff);
-        } else {
-          res.status(404).json(errorMessages.noHotel);
-        }
+routes.get('/', restricted(config, access.admins), async (req, res, next) => {
+  try {
+    const hotelId = req.query.hotel_id;
+    if (hotelId) {
+      const hotel = await models.Hotel.findById(hotelId);
+      if (hotel) {
+        // if hotel id exists, find all users with that hotel ID and return them
+        const hotelUsers = await models.User.where({
+          hotel_id: hotelId
+        }).where({ is_left: false });
+        // filter only hotel staff
+        const hotelStaff = hotelUsers.filter(
+          user => user.user_type !== USER_TYPES.GUEST
+        );
+        res.status(200).json(hotelStaff);
       } else {
-        res.status(404).json(errorMessages.missingQueryString);
+        res.status(404).json(errorMessages.noHotel);
       }
-    } catch (error) {
-      next(error);
+    } else {
+      res.status(404).json(errorMessages.missingQueryString);
     }
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 // this endpoint is not used on FE
 routes.get(
   '/:_id',
   validateObjectId,
-  restricted(config, access.superAdmin),
+  restricted(config, access.admins),
   async (req, res, next) => {
     try {
       const user = await models.User.findById(req.params._id);
@@ -123,43 +119,47 @@ routes.post(
   }
 );
 
-routes.put('/:_id', validateObjectId, async (req, res, next) => {
-  const { _id } = req.params;
-  const incomingUser = { ...req.body };
-  if (incomingUser.password) {
-    incomingUser.password = bcrypt.hashSync(incomingUser.password, 10);
-  }
-  try {
-    const user = await models.User.findById({ _id });
-
-    if (user) {
-      if (
-        // three requests are valid:
-        // 1) an update with a new email address, that is not the same as any other user in the db
-        // 2) an update with an email address the same as the users current email
-        // 3) a non-user-settings update with no email address
-        !(await documentExists({ email: incomingUser.email }, 'User')) ||
-        user.email === incomingUser.email ||
-        !incomingUser.email
-      ) {
-        // check for `incomingUser` properties and update them in the `user` object
-        updateUser(user, incomingUser);
-
-        const result = await user.save();
-        const resultWithoutPassword = { ...result._doc };
-        delete resultWithoutPassword.password;
-        res.status(200).json(resultWithoutPassword);
-      } else {
-        res.status(402).json(errorMessages.duplicateEmail);
-      }
-    } else {
-      res.status(404).json(errorMessages.getUserById);
+routes.put(
+  '/:_id',
+  validateObjectId,
+  restricted(config, access.hotelStaff),
+  async (req, res, next) => {
+    const { _id } = req.params;
+    const incomingUser = { ...req.body };
+    if (incomingUser.password) {
+      incomingUser.password = bcrypt.hashSync(incomingUser.password, 10);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+    try {
+      const user = await models.User.findById({ _id });
 
+      if (user) {
+        if (
+          // three requests are valid:
+          // 1) an update with a new email address, that is not the same as any other user in the db
+          // 2) an update with an email address the same as the users current email
+          // 3) a non-user-settings update with no email address
+          !(await documentExists({ email: incomingUser.email }, 'User')) ||
+          user.email === incomingUser.email ||
+          !incomingUser.email
+        ) {
+          // check for `incomingUser` properties and update them in the `user` object
+          updateUser(user, incomingUser);
+
+          const result = await user.save();
+          const resultWithoutPassword = { ...result._doc };
+          delete resultWithoutPassword.password;
+          res.status(200).json(resultWithoutPassword);
+        } else {
+          res.status(402).json(errorMessages.duplicateEmail);
+        }
+      } else {
+        res.status(404).json(errorMessages.getUserById);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 routes.delete(
   '/:_id',
